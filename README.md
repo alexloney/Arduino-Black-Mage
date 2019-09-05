478c9692dbc3b037da2dd3aa9685202b818d228e
Alex Loney <loney.alex@gmail.com>
2019-09-05 16:12:50 -0700

    Migrating away from a button pad to a bluetooth adapter

--- README.md
+++ README.md
@@ -2,13 +2,12 @@
 This is the source code for my Black Mage cosplay with LED eyes.
 
 ## Hardware
-* [Ardiono Uno ($22)](https://store.arduino.cc/usa/arduino-uno-rev3)
+* [FLORA - Wearable electronic platform ($14.95)](https://www.adafruit.com/product/659)
 * [2xAdafruit NeoPixel Shield for Arduino ($55.90)](https://www.adafruit.com/product/1430)
-* [Membrane 1x4 Keypad ($2.95)](https://www.adafruit.com/product/1332)
 * [Stainless Tin Conductive Thread ($6.95)](https://www.adafruit.com/product/640)
-* [9V battery clip with 5.5mm/2.1mm plug ($2.95)](https://www.adafruit.com/product/80)
+* [Flora Wearable Bluefruit LE Module ($17.50)](https://www.adafruit.com/product/2487)
 
-Total cost: $90.75
+Total cost: $95.30
 
 ## Curcit Layout
 
---
+++ main/main.ino
@@ -0,0 +1,152 @@
+#include "eyes.h"
+
+#include <string.h>
+#include <Arduino.h>
+#include <SPI.h>
+#include "Adafruit_BLE.h"
+#include "Adafruit_BluefruitLE_SPI.h"
+#include "Adafruit_BluefruitLE_UART.h"
+
+#include "BluefruitConfig.h"
+
+#if SOFTWARE_SERIAL_AVAILABLE
+  #include <SoftwareSerial.h>
+#endif
+
+#define FACTORYRESET_ENABLE         0
+#define MINIMUM_FIRMWARE_VERSION    "0.6.6"
+#define MODE_LED_BEHAVIOUR          "MODE"
+
+#define LEFT_EYE_PIN 9
+#define RIGHT_EYE_PIN 10
+
+unsigned long nextBlink = 0;
+int currentEyeState = STATE_NORMAL;
+
+Eye *leftEye;
+Eye *rightEye;
+
+Adafruit_BluefruitLE_UART ble(BLUEFRUIT_HWSERIAL_NAME, BLUEFRUIT_UART_MODE_PIN);
+
+void error(const __FlashStringHelper*err) {
+  Serial.println(err);
+  while (1);
+}
+
+// function prototypes over in packetparser.cpp
+uint8_t readPacket(Adafruit_BLE *ble, uint16_t timeout);
+float parsefloat(uint8_t *buffer);
+void printHex(const uint8_t * data, const uint32_t numBytes);
+
+// the packet buffer
+extern uint8_t packetbuffer[];
+
+void setup() {
+  randomSeed(analogRead(0));
+  
+  // while (!Serial);  // required for Flora & Micro
+  delay(500);
+  
+  Serial.begin(115200);
+  
+  /* Initialise the module */
+  Serial.print(F("Initialising the Bluefruit LE module: "));
+
+  if ( !ble.begin(VERBOSE_MODE) )
+  {
+    error(F("Couldn't find Bluefruit, make sure it's in CoMmanD mode & check wiring?"));
+  }
+  Serial.println( F("OK!") );
+  
+  if ( FACTORYRESET_ENABLE )
+  {
+    /* Perform a factory reset to make sure everything is in a known state */
+    Serial.println(F("Performing a factory reset: "));
+    if ( ! ble.factoryReset() ){
+      error(F("Couldn't factory reset"));
+    }
+  }
+
+
+  /* Disable command echo from Bluefruit */
+  ble.echo(false);
+
+  Serial.println("Requesting Bluefruit info:");
+  /* Print Bluefruit information */
+  ble.info();
+
+  Serial.println(F("Please use Adafruit Bluefruit LE app to connect in Controller mode"));
+  Serial.println(F("Then activate/use the sensors, color picker, game controller, etc!"));
+  Serial.println();
+
+  ble.verbose(false);  // debug info is a little annoying after this point!
+
+  /* Wait for connection */
+  while (! ble.isConnected()) {
+      delay(500);
+  }
+
+  Serial.println(F("******************************"));
+
+  // LED Activity command is only supported from 0.6.6
+  if ( ble.isVersionAtLeast(MINIMUM_FIRMWARE_VERSION) )
+  {
+    // Change Mode LED Activity
+    Serial.println(F("Change LED activity to " MODE_LED_BEHAVIOUR));
+    ble.sendCommandCheckOK("AT+HWModeLED=" MODE_LED_BEHAVIOUR);
+  }
+
+  // Set Bluefruit to DATA mode
+  Serial.println( F("Switching to DATA mode!") );
+  ble.setMode(BLUEFRUIT_MODE_DATA);
+
+  Serial.println(F("******************************"));
+  
+  leftEye = new Eye(LEFT_EYE, LEFT_EYE_PIN);
+  rightEye = new Eye(RIGHT_EYE, RIGHT_EYE_PIN);
+
+  nextBlink = millis() + random(1000 * 90, 1000 * 120);
+}
+
+bool buttons[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+
+void loop() {
+  unsigned long currentTime = millis();
+  uint8_t len = readPacket(&ble, BLE_READPACKET_TIMEOUT);
+
+  if (packetbuffer[1] == 'B') {
+    uint8_t buttnum = packetbuffer[2] - '0';
+    bool pressed = packetbuffer[3] - '0';
+    if (pressed) {
+      buttons[buttnum] = 1;
+    }
+    else {
+      buttons[buttnum] = 0;
+    }
+  }
+  
+  if (currentTime > nextBlink)
+  {
+    if (leftEye->getState() == STATE_NORMAL)
+    {
+      leftEye->closedEye();
+      rightEye->closedEye();
+      delay(150);
+      leftEye->normalEye();
+      rightEye->normalEye();
+    }
+    
+    nextBlink = millis() + random(1000 * 90, 1000 * 120);
+  }
+
+  if (buttons[1])
+  {
+    leftEye->happyEye();
+    rightEye->happyEye();
+  }
+  else
+  {
+    leftEye->normalEye();
+    rightEye->normalEye();
+  }
+}
